4eb83ea568ddd0f34d96ce7da72150ce4d8a5460
Diego Flores <diegoflores1991@gmail.com>
2016-12-07 17:09:18 -0300

    Update preinstall

--- preinstalls.sh
+++ preinstalls.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # link NODE and NPM
-ln -sf `ls -td /opt/elasticbeanstalk/node-install/node-* | head -1`/bin/node /bin/node
-ln -sf `ls -td /opt/elasticbeanstalk/node-install/node-* | head -1`/bin/npm /bin/npm
+PATH=$PATH:`ls -td /opt/elasticbeanstalk/node-install/node-* | head -1`/bin
 
 # Install bower 
 npm i -g bower
